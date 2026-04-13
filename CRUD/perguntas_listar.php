f6f24d7bcbca3c391784454d3d7bcaeed1ea479a
DanFischer01 <miojo1412@gmail.com>
2026-04-13 11:26:27 -0300

    Create perguntas_listar.php

---
+++ CRUD/perguntas_listar.php
@@ -0,0 +1,31 @@
+<?php
+require_once 'config.php';
+$perguntas = lerPerguntas();
+?>
+<!DOCTYPE html>
+<html>
+<body>
+    <h2>Lista de Perguntas</h2>
+    <table border="1">
+        <tr>
+            <th>ID</th>
+            <th>Tipo</th>
+            <th>Enunciado</th>
+            <th>Ações</th>
+        </tr>
+        <?php foreach ($perguntas as $p): ?>
+        <tr>
+            <td><?php echo $p['id']; ?></td>
+            <td><?php echo $p['tipo']; ?></td>
+            <td><?php echo substr($p['enunciado'], 0, 50); ?>...</td>
+            <td>
+                <a href="pergunta_detalhe.php?id=<?php echo $p['id']; ?>">Visualizar</a> | 
+                <a href="perguntas_editar.php?id=<?php echo $p['id']; ?>">Editar</a> | 
+                <a href="perguntas_excluir.php?id=<?php echo $p['id']; ?>" onclick="return confirm('Excluir?')">Excluir</a>
+            </td>
+        </tr>
+        <?php endforeach; ?>
+    </table>
+    <br><a href="index.php">Voltar ao Menu</a>
+</body>
+</html>
