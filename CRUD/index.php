b7b964be8b169a5dc696f77eaf4dfc58624df744
DanFischer01 <miojo1412@gmail.com>
2026-04-13 11:04:51 -0300

    Add files via upload

---
+++ CRUD/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Daniel Fischer - AV1 - CAW: Serviço para Water Falls</title>
+</head>
+<body>
+    <h1>Sistema de perguntas e respostas</h1>
+    <nav>
+        <ul>
+            <li><a href="perguntas_cadastrar.php?tipo=multipla">Criar Pergunta Múltipla Escolha</a></li>
+            <li><a href="perguntas_cadastrar.php?tipo=texto">Criar Pergunta de Texto</a></li>
+            <li><a href="perguntas_listar.php">Listar Todas as Perguntas</a></li>
+        </ul>
+    </nav>
+</body>
+</html>
